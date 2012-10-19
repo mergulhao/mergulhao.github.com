f03971bcf3ee624e20bae88ec669ca99bbf0b1e0
RamonPage <ramonpage@gmail.com>
2012-10-19 18:10:11 -0300

    Adding first styles and scripts for rel='external' and scroll to top.

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,1 +1,24 @@
 // Scripts
+
+$(window).scroll(function(e) {
+  var top = $(document).scrollTop();
+
+  if (top > 100) {
+    $("#go-to-top").fadeIn("slow");
+  } else {
+    $("#go-to-top").fadeOut("slow");
+  }
+});
+
+$(function() {
+  $("a[rel^='external']").each(function() {
+    $(this).attr("target","_blank");
+  });
+
+  $("#go-to-top a").click(function (e){
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("section").offset().top
+    }, 1000);
+  });
+});
